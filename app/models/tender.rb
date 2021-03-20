15c9ae14d816c7f6f9be7e2429f1a0ed1f0f96c8
Luke Billings <lukecbillings@gmail.com>
2021-03-20 21:32:41 +0000

    got seeds working

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -1,3 +1,4 @@
 class Tender < ApplicationRecord
     belongs_to :user
+    has_many :bids, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+require "open-uri"
+Bid.destroy_all
+Tender.destroy_all
+User.destroy_all
+
+
+
+#bar users
+10.times do
+  user = User.create!(name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "123456",
+    brand: false,
+    bar: true,
+    about: "We are a trendy bar in East London"
+    )
+end
+
+#brand users
+
+10.times do
+  user = User.create!(name: Faker::Company.name,
+    email: Faker::Internet.email,
+    password: "123456",
+    brand: true,
+    bar: false,
+    about: "We are a drinks startup"
+    )
+end
+
+
+
+#tenders
+
+
+10.times do
+  tender = Tender.create!(deal_size: rand(1000..5000),
+    volume: rand(1000..5000),
+    deal_length: rand(4..24),
+    end_date: DateTime.new(2021, 06, 12, 18, 00, 0),
+    total_bids: rand(1..10),
+    user_id: User.pluck(:id).sample
+    )
+end
+
+
+#bids
+
+
+10.times do
+  bid = Bid.create!(listing_deal: "We can give you £500 and 10 posters",
+    user_id: User.pluck(:id).sample,
+    tender_id: Tender.pluck(:id).sample,
+    accepted: false
+    )
+end
